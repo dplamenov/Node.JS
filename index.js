e03939e23e0a81549d48206eef264bb53fc14802
Dimitar Plamenov Dimitrov <dimitar.plamenov@gmail.com>
2020-03-02 15:03:55 +0200

    install twig template engine

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const application = express();
-
+application.set('views', './views') // specify the views directory
+application.set('view engine', 'twig') // register the template engine
 application.get('/', function (request, respone, next) {
-    respone.send('Hi!');
+    respone.render('index', { msg: "Welcome!" });
 });
 
 application.listen(8080);
