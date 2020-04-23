64bbc6acaf0f775e022b8efcb590c2b3941ef01e
Dimitar Plamenov Dimitrov <dimitar.plamenov@gmail.com>
2020-04-23 16:12:57 +0300

    in index view add links to change status of tasks

--- Models/Task.js
+++ Models/Task.js
@@ -2,10 +2,12 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const taskSchema = new Schema({
-    title: String,
+    title: { type: String, required: true },
     body: String,
     status: String,
     dueDate: String
+}, {
+    versionKey: false
 });
 
 const task = mongoose.model('Task', taskSchema);
