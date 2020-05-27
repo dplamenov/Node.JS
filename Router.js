2cafbb25f8321f7c93d8249c84389e843b7bee50
Dimitar Plamenov Dimitrov <dimitar.plamenov@gmail.com>
2020-05-27 09:22:13 +0300

    Added user logout functionality

--- Router.js
+++ Router.js
@@ -4,8 +4,8 @@ const router = express.Router();
 let taskController = require('./Controllers/taskController.js');
 let userController = require('./Controllers/userController.js');
 
-router.get('/', function (request, response, next) {
-    taskController.index(request, response, next);
+router.get('/', function (request, response) {
+    taskController.index(request, response);
 });
 router.post('/', function (request, response, next) {
     taskController.saveTask(request, response, next);
@@ -16,6 +16,7 @@ router.get('/setStatus/:taskId/:status', function (request, response, next) {
 router.get('/delete/:taskId', function (request, response, next) {
     taskController.deleteTask(request, response, next);
 });
-router.get('/login', (request, response) => { if (request.session.isLogin) { return response.redirect('/') } response.render('loginForm') })
-router.post('/login', (request, response, next) => { userController.login(request, response, next) })
+router.get('/login', (request, response) => { if (request.session.isLogin) { return response.redirect('/') } response.render('loginForm') });
+router.post('/login', (request, response, next) => { userController.login(request, response, next) });
+router.get('/logout', (request, response) => { request.session.isLogin = false; return response.redirect('/') });
 module.exports = router;
